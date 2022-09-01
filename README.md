f4b420b390868407961c0a0ba257ae55603a37c2
suheiryrivera <suheiry.rivera@upr.edu>
2022-08-31 21:45:54 -0400

    sum result

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Lab3-prac
 
 I hope Ill do well in this lab .-.
+
---
+++ code.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main(){
+
+int a,b,c,d;
+
+a = 3;
+b = 4;
+c = 10;
+
+d = a+b+c;
+
+cout<<d<<endl;
+
+return 0;
+
+
+}
